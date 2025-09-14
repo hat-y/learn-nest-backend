e3268f375c0d232834629c65a6ee425fd3f35ef2
Facundo Gayoso <facusbrr@gmail.com>
2025-09-14 02:03:54 -0300

    style(app) modified code in both

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from './prisma/prisma.module';
+import { PrismaService } from './prisma/prisma.service';
+import { UsersService } from './src/users/users.service';
+import { UsersService } from './users/users.service';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule],
   controllers: [],
-  providers: [],
+  providers: [PrismaService, UsersService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // hook for make run to onModuleDestroy()
+  app.enableShutdownHooks();
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
